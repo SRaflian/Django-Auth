103dfb3a1de1a55ac88df296fb105ff2ee8c578c
SRaflian <sraflian@outlook.com>
2024-02-04 06:31:55 +0700

    customizing registration page

--- django_project/accounts/urls.py
+++ django_project/accounts/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
-from .views import SignUpView
 
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
     path('register/', views.register, name='register')
 ]
